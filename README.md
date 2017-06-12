f806ba256358938f25f53072ccb976ea65154361
lennygallagher <lenny.gallagher@gmail.com>
2017-06-12 14:42:59 +0200

    gui as Dockerimage (bad)

--- README.md
+++ README.md
@@ -6,4 +6,14 @@
 #Run GUI
 im gui projekt:
 - npm install
+- sudo npm install -g browserify
+- sudo npm install -g minifier
+- sudo npm install -g @angular/cli
 - npm start
+
+#Docker image erzeugen u. run
+create image and run:
+- execute ./build-run.sh
+
+stop:
+- execute ./stopGui.sh
---
+++ build-run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+if [ $# -eq 0 ]
+  then
+    echo "ERROR: Docker version for GUI is required as argument. E.q. 1.0.0"
+    exit 1;
+fi
+npm run clean
+cp -r src/* dist
+cp -r node_modules dist
+docker build -t lenny/gui:${1} .
+docker stop gui
+docker rm gui
+docker run -d --name gui -p 3000:80 lenny/gui:${1}
