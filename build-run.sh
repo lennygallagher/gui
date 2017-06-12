eb4ef3b55d67c8a88bebf17d655742e2f734c9ef
lennygallagher <lenny.gallagher@gmail.com>
2017-06-12 17:22:49 +0200

    gui in hackton netz

--- build-run.sh
+++ build-run.sh
@@ -10,4 +10,4 @@ cp -r node_modules dist
 docker build -t lenny/gui:${1} .
 docker stop gui
 docker rm gui
-docker run -d --name gui -p 3000:80 lenny/gui:${1}
+docker run -d --name gui --net=hackathon -p 3000:80 lenny/gui:${1}
